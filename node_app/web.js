e9675cb541d6ff71b3d9c3c0d8bc0f4329f92b49
James Magnarelli <jmagnare@gmail.com>
2014-03-30 03:55:26 -0400

    Add and cancel now working with new schema

--- node_app/web.js
+++ node_app/web.js
@@ -6,43 +6,62 @@ var twilioSecrets = require('./twilioSecrets.js');
 var app = express();
 app.use(express.logger());
 
+
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+
 var firebase = new Firebase('https://glowing-fire-3800.firebaseio.com')
 var twilioClient = new twilio.RestClient(twilioSecrets.sid, twilioSecrets.authToken);
-var emergencyMessage = "OH NOES - YOUR FRIEND IS MISSING! SORRY, DUDE."
+var emergencyMessage = "Hi! %UserFirstName may need your help! They didn't check in with Mamoru and their timer has expired. We hope everything's okay.";
 
-var sendTwilioMessages = function(phone_numbers) {
-	// Send the twilio messages
-	phone_numbers.forEach(function(to_number) {
+var buildEmergencyMessage = function(userFirstName, toFirstName) {
+	return "Hi " + userFirstName + " - " + toFirstName + " may need your help! They didn't check in with Mamoru and their timer has expired. We hope everything's okay.";
+}
+
+var sendTwilioMessages = function(userName, recipients) {
+	/*
+	Send panic messages to the given phone numbers and names
+
+	NOTE: toPhoneNumbers.length must equal toFirstNames.length
+	*/
+	recipients.forEach(function(recipient) {
 		twilioClient.sms.messages.create({
-		    to:to_number,
-		    from:'13399703245',
-		    body:emergencyMessage
+		    to: recipient['number'],
+		    from: twilioSecrets.fromNumber,
+		    body: buildEmergencyMessage(userName, recipient['name'])
 		});
+		console.log("Sent message");
 	});
 }
 
 // TODO (jmagnarelli): come up with a better way to store these
 var callback_objs = [];
 
-firebase.on('child_added', function(newValue) {
-	callback_objs[newValue.name()] = setTimeout(sendTwilioMessages, 10000, ['9786048120'])//newValue.val()['delay'], newValue.val()['recipients']['phone'])
-	if (callback_objs[newValue.name()]) {
-		newValue.ref().update({'state': 'SERVER_TIMER_SET'});
-	}
-	
+var timerRequestsRef = firebase.child('timerRequests');
+
+// Assume name is device id
+var userSettingsRef = firebase.child('userSettings');
+
+timerRequestsRef.on('child_added', function(newValue) {
+	userSettingsRef.child(newValue.name()).on('value', function(snapshot) {
+		callback_objs[newValue.name()] = setTimeout(sendTwilioMessages, newValue.val()['timerLength'], snapshot.val()['firstName'], snapshot.val()['contacts'])
+		if (callback_objs[newValue.name()]) {
+			newValue.ref().update({'state': 'SERVER_TIMER_SET'});
+			console.log("Adding message timer");
+		}
+	})
 });
 
-firebase.on('child_changed', function(newValue) {
-	if (callback_objs[newValue.name()] != undefined) {
-		clearTimeout(callback_objs[newValue.name()]);
-		delete callback_objs[newValue.name()]; // Otherwise we'll loop forever
-		newValue.ref().update({'state': 'SERVER_TIMER_CANCELED'});
+timerRequestsRef.on('child_removed', function(snapshot) {
+	if (callback_objs[snapshot.name()] != undefined) {
+		clearTimeout(callback_objs[snapshot.name()]);
+		delete callback_objs[snapshot.name()]; // Otherwise we'll loop forever
+		console.log("Removing message timer");
 	}
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
